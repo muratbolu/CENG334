544c393b811c8d7092da4f38dd7b805e3937050f
muratbolu <muratbolu2000@gmail.com>
2024-05-06 19:38:49 +0300

    Separate classes into different files

--- HW2/include/simulator.hpp
+++ HW2/include/simulator.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "car.hpp"
+#include "crossroad.hpp"
+#include "ferry.hpp"
+#include "narrow_bridge.hpp"
+
 #include <cstdint>
-#include <monitor.h>
 #include <pthread.h>
-#include <string>
-#include <variant>
 #include <vector>
 
 class Simulator
@@ -24,55 +26,15 @@ class Simulator
     using u8 = std::uint8_t;
     using u32 = std::uint32_t;
 
+    friend Car;
+
     void parse_input() noexcept;
     void create_car_threads() noexcept;
     void join_car_threads() const noexcept;
 
-    struct NarrowBridge : public Monitor
-    {
-        u32 travel_time;
-        u32 maximum_wait_time;
-    };
     std::vector<NarrowBridge> narrow_bridges;
-
-    struct Ferry : public Monitor
-    {
-        u32 travel_time;
-        u32 maximum_wait_time;
-        u32 capacity;
-    };
     std::vector<Ferry> ferries;
-
-    struct Crossroad : public Monitor
-    {
-        u32 travel_time;
-        u32 maximum_wait_time;
-    };
     std::vector<Crossroad> crossroads;
-
-    struct Car
-    {
-        using connector_ptr = std::variant<NarrowBridge*, Ferry*, Crossroad*>;
-
-        Simulator* sim;
-        u32 travel_time;
-
-        struct Destination
-        {
-            connector_ptr connector_type;
-            u32 from;
-            u32 to;
-        };
-        std::vector<Destination> path;
-
-        // Do not mark noexcept!
-        static void* car_routine(void*);
-
-        void get_path() noexcept;
-        [[nodiscard]] connector_ptr constexpr to_connector(
-          const std::string&) const noexcept;
-        [[nodiscard]] u32 static constexpr to_uint(const char&) noexcept;
-    };
     std::vector<Car> cars;
     std::vector<pthread_t> car_threads;
 };
--- HW2/src/simulator.cpp
+++ HW2/src/simulator.cpp
@@ -1,13 +1,15 @@
 #include "simulator.hpp"
 
 #include "WriteOutput.h"
+#include "car.hpp"
+#include "crossroad.hpp"
+#include "ferry.hpp"
+#include "narrow_bridge.hpp"
 
-#include <cassert>
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <pthread.h>
-#include <string>
 
 void Simulator::run() noexcept
 {
@@ -63,54 +65,4 @@ void Simulator::join_car_threads() const noexcept
     {
         pthread_join(t, nullptr);
     }
-}
-
-void* Simulator::Car::car_routine(void* arg)
-{
-    assert(arg != nullptr);
-    Car& car{ *static_cast<Car*>(arg) };
-    pthread_exit(nullptr);
-}
-
-void Simulator::Car::get_path() noexcept
-{
-    u32 path_length;
-    std::cin >> path_length;
-    path.resize(path_length);
-    for (auto& p : path)
-    {
-        std::string connector_str;
-        std::cin >> connector_str >> p.from >> p.to;
-        p.connector_type = to_connector(connector_str);
-    }
-}
-
-[[nodiscard]] Simulator::Car::connector_ptr constexpr Simulator::Car::
-  to_connector(const std::string& str) const noexcept
-{
-    switch (str[0])
-    {
-        case 'N':
-        {
-            return &sim->narrow_bridges[to_uint(str[1])];
-        }
-        case 'F':
-        {
-            return &sim->ferries[to_uint(str[1])];
-        }
-        case 'C':
-        {
-            return &sim->crossroads[to_uint(str[1])];
-        }
-        default:
-        {
-            assert(0 && "unreachable");
-        }
-    }
-}
-
-[[nodiscard]] Simulator::u32 constexpr Simulator::Car::to_uint(
-  const char& c) noexcept
-{
-    return static_cast<u32>(c - '0');
 }
