d2d7fd645dcb9724bb96b9242019c268cdfad638
muratbolu <muratbolu2000@gmail.com>
2024-06-01 20:14:13 +0300

    Implement parsing

--- HW3/include/recext2fs.hpp
+++ HW3/include/recext2fs.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
+#include <vector>
 
 class recext2fs
 {
    public:
+    using u8 = std::uint8_t;
+
     explicit recext2fs(int argc, char* argv[]) noexcept;
 
    private:
     std::string image_location;
-    std::string data_identifier;
+    std::vector<u8> data_identifier;
+
+    std::vector<u8> static parse_identifier(int argc, char* argv[]) noexcept;
 };
--- HW3/src/recext2fs.cpp
+++ HW3/src/recext2fs.cpp
@@ -1,16 +1,36 @@
 #include "recext2fs.hpp"
 
+#include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
+using u8 = recext2fs::u8;
 
 recext2fs::recext2fs(int argc, char* argv[]) noexcept
 {
-    if (argc != 3)
+    if (argc < 3)
     {
         std::cerr << "Usage: " << argv[0]
                   << " <image_location> <data_identifier>" << std::endl;
         return;
     }
     image_location = std::string{ argv[1] };
-    data_identifier = std::string{ argv[2] };
+    data_identifier = parse_identifier(argc, argv);
+
+    std::ifstream image{ image_location };
+}
+
+std::vector<u8> recext2fs::parse_identifier(int argc, char* argv[]) noexcept
+{
+    int identifier_length{ argc - 2 };
+    std::vector<u8> identifier;
+    for (int i{ 0 }; i < identifier_length; ++i)
+    {
+        unsigned temp{ 0 };
+        sscanf(argv[i + 2], "%x", &temp);
+        identifier.emplace_back(temp);
+    }
+    return identifier;
 }
